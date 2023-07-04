6cd00a69cb36e0cfe55339fec02b334060be2d03
Kamshory <kamshory@yahoo.com>
2023-07-04 15:49:14 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-date_default_timezone_set("Asia/Bangkok");
+date_default_timezone_set("Asia/Jakarta");
 require_once "vendor/autoload.php";
 $wss = new ChatServer('127.0.0.1', 8888);
 $wss->run();
